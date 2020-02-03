d0431b19fef284ec6046036f7d585bb1f0dd7c6f
romanornr <romanornr@gmail.com>
2020-02-03 16:57:08 +0100

    RPC basics

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,1 +1,44 @@
 package rpc
+
+import (
+	"github.com/btcsuite/btcd/rpcclient"
+	"net/http"
+	"net/url"
+	"sync"
+	"sync/atomic"
+)
+
+type RPCClient struct {
+	sync.RWMutex
+	sickRate         int64
+	successRate      int64
+	Accepts          int64
+	Rejects          int64
+	LastSubmissionAt int64
+	FailsCount       int64
+	Url              *url.URL
+	login            string
+	password         string
+	Name             string
+	sick             bool
+	client           *http.Client
+	info             atomic.Value
+}
+
+func NewRPCCient() (*rpcclient.Client, error){
+	connCfg := &rpcclient.ConnConfig{
+		Host:         "127.0.0.1:5222",
+		User:         "via",
+		Pass:         "via",
+		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
+		DisableTLS:   true, // Bitcoin core does not provide TLS by default
+	}
+
+	client, err := rpcclient.New(connCfg, nil)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+
--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -1,1 +1,35 @@
 package rpc
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+type BlockTemplateReply struct {
+	Difficulty     int64  `json:"difficulty"`
+	Height         int64  `json:"height"`
+	Blob           string `json:"blocktemplate_blob"`
+	ReservedOffset int    `json:"reserved_offset"`
+	PrevHash       string `json:"prev_hash"`
+}
+
+func TestNewRPCCient(t *testing.T) {
+	r, err := NewRPCCient()
+	if err != nil {
+		t.Fail()
+	}
+
+	_, err = r.GetBlockCount()
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestGetBlockTemplate(t *testing.T) {
+	r, _  := NewRPCCient()
+
+	_, err := r.RawRequest("getblocktemplate", []json.RawMessage{})
+	if err != nil {
+		t.Fail()
+	}
+}
